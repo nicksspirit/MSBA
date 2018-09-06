e666a16f32695be76b8681f20606f5e02920b524
Nick Spirit <nickspirit3+github@gmail.com>
2018-09-05 21:05:31 -0400

    * Completed bootcamp assignment using jupyter notebook

---
+++ 64018/RNotebook/boot camp_assignment.r
@@ -0,0 +1,22 @@
+
+company_df <- read.csv(
+  file = file.choose(),
+  header = T,
+  sep = ",",
+  na.strings = "?",
+  stringsAsFactors = F
+)
+
+company_df[1:5, ]
+
+summary(company_df)
+
+sprintf("The total number of employees in Happy Co. is %d", nrow(company_df))
+
+plot(
+  x = company_df$YearsExperience,
+  y = company_df$Salary,
+  xlab = "Experience",
+  ylab = "Salary",
+  main = "Salary vs Experience"
+)
